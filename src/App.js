eab9465089d819196d18f643f80ad04ad0ba242b
beskorovainyi <beskorovainyi.max@gmail.com>
2022-05-25 22:48:22 +0300

    ADDED:added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import BuyOnlinePage from "./views/BuyOnlinePage"
+import HomePage from "./views/HomePage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={() => <HomePage />}/>
+        <Route path="/buy-online"  component={() => <BuyOnlinePage />}/>
+      </Switch>
+    
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/views/BuyOnlinePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const BuyOnlinePage = () => {
+
+  return(
+    <>
+      <h2>Buy online</h2>
+    </>
+  )
+}
+
+export default BuyOnlinePage
